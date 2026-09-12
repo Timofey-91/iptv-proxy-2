945ca74af614396b73d2f060bcde34c9d07ed16a
Timofey <50114291+Timofey-91@users.noreply.github.com>
2026-09-12 21:46:03 +0500

    Update update_config.py

--- update_config.py
+++ update_config.py
@@ -11,7 +11,7 @@ CONFIG_FILE = "config.json"
 def get_token():
     """Получаем access_token с PeersTV"""
 
-    url = "http://api-spb.peers.tv/auth/2/token"
+    url = "http://api-nsk.peers.tv/auth/2/token"
 
     payload = (
         "grant_type=inetra%3Aanonymous"
@@ -56,7 +56,7 @@ def get_stream_url(channel, channel_id, token, offset):
     """Формируем оригинальную ссылку PeersTV"""
 
     base_url = (
-        f"http://api.peers.tv/"
+        f"http://api-nsk.peers.tv/"
         f"timeshift/{channel}/{channel_id}/playlist.m3u8"
     )
 
