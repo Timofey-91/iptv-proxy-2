f4abbda9ca941a8fb5c3d176398723a0a7e425ba
Timofey91 <timati-77777@yandex.ru>
2026-09-12 12:03:30 +0000

    Update: update_config.py

--- update_config.py
+++ update_config.py
@@ -14,7 +14,7 @@ def get_token():
     url = "http://api-spb.peers.tv/auth/2/token"
 
     payload = (
-        "grant_type=inetra"
+        "grant_type=inetra%3Aanonymous"
         "&client_id=29783051"
         "&client_secret=b4d4eb438d760da95f0acb5bc6b5c760"
     )
